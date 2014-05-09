6c77f816088fcacd7ba0cd5cf43a4ffba914cc7c
Ervis <ervistusha@gmail.com>
2014-05-09 22:54:31 +0200

    check if xrandr is installed

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QProcess>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QProcess process;
+    process.start("which xrandr");
+    process.waitForFinished();
+    QString output;
+    output =  process.readAllStandardOutput();
+    if (output==""){
+        QMessageBox msgBox;
+        msgBox.setIcon(QMessageBox::Critical);
+        msgBox.setText("Please install XRandR");
+        msgBox.setInformativeText("<a href=\"http://www.x.org/wiki/Projects/XRandR/\">http://www.x.org/wiki/Projects/XRandR/</a>");
+        msgBox.setStandardButtons(QMessageBox::Close);
+        msgBox.exec();
+        return 0 ;
+    }
+
     MainWindow w;
     w.show();
 
